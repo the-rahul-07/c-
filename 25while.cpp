97e4c6ef5e49c2da9c48052e3b9c5af8e1598e53
Rahul <rahulgupta2019007@gmail.com>
2026-01-06 21:48:24 +0530

    while loop

---
+++ 25while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std ::cin;
+using std ::cout;
+
+int main()
+{
+    int i = 10;
+    while (i > 0)
+    {
+        cout << i << "\t";
+        i--;
+    }
+    return 0;
+}
