6a5add6bff5c01b2eba4a967152001b258bc2a7d
Rahul <rahulgupta2019007@gmail.com>
2026-01-09 17:30:57 +0530

    multidimensional array

---
+++ 43multidemarray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std ::cin;
+using std ::cout;
+
+int main()
+{
+    int matrix[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j] << " \t";
+        }
+        cout << " \n";
+    }
+
+    return 0;
+}
